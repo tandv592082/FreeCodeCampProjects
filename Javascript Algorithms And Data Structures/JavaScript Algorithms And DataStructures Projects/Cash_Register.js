b0a7428de35c2b25dfd5673c26d8c0c7eb0479ab
Tandv195 <mchemistry95@gmail.com>
2019-04-18 20:14:37 +0700

    Update Cash_Register.js

--- Javascript Algorithms And Data Structures/JavaScript Algorithms And DataStructures Projects/Cash_Register.js
+++ Javascript Algorithms And Data Structures/JavaScript Algorithms And DataStructures Projects/Cash_Register.js
@@ -31,7 +31,7 @@ function checkCashRegister(price, cash, cid) {
         var currentUnit = 0;
         units.map((el, index) => {
             while(change >= el.value && currentUnit < cid[9 - index - 1][1]){
-                change = Math.round((change - el.value) *100) / 100;
+                change = Math.round((change -  el.value)*100)/100;
                 currentUnit += el.value;
                 
             }
@@ -63,4 +63,4 @@ function checkCashRegister(price, cash, cid) {
   // ["TWENTY", 60],
   // ["ONE HUNDRED", 100]]
   
-console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
+console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
