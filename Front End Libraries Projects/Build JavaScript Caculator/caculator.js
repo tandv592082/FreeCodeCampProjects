8f02814401b5dfb401194b79e8366320d67f7212
Tân Dv <mchemistry95@gmail.com>
2019-04-27 16:51:36 +0700

    add some function

--- Front End Libraries Projects/Build JavaScript Caculator/caculator.js
+++ Front End Libraries Projects/Build JavaScript Caculator/caculator.js
@@ -18,26 +18,45 @@ let deleteAll = () => {
 let next = false;
 let insert = (num) => {
     let arr = document.querySelectorAll("[id='textview']");
-    arr[0].value += num;
-    if(start){
+    //console.log('next:'+next)
+    if(change){
+        arr[0].value = result + num;
         arr[1].value = num;
+        change = false;
     }
     else{
         if(num === '+' || num === '-' ||num === '*' ||num === '/'){
-            arr[1].value = num;
-            next = true;
-        }
-        else{
             if(next){
-                arr[1].value = num;
+                let newStr = arr[0].value;
+                arr[0].value = newStr.substring(0,newStr.length - 1) + num;
             }
             else{
-                arr[1].value += num;
+                arr[0].value += num;
             }
-            next = false;
+        }
+        else{
+            arr[0].value += num;
+        }
+        if(start){
+            arr[1].value = num;
+            start = false;
+        }
+        else{
+            if(num === '+' || num === '-' ||num === '*' ||num === '/'){
+                arr[1].value = num;
+                next = true;
+            }
+            else{
+                if(next){
+                    arr[1].value = num;
+                }
+                else{
+                    arr[1].value += num;
+                }
+                next = false;
+            }
         }
     }
-    
 }
 
 let handler = (str) => {
@@ -51,10 +70,12 @@ let handler = (str) => {
         if(first === '-') check = true;
         str = str.substring(1,str.length);
     }  
-    let arr = str.split(/[*-+/]/g);
+    let arr = str.split(/[-*+/]/g);
+    console.log(arr)
     if(check){
         arr[0] = '-' + arr[0];
     }
+    console.log(arr)
     let index = 1;
     for(let i = 0; i < str.length; i++){
         if(str.charAt(i) === '*' || str.charAt(i) === '+' || str.charAt(i) === '-' || str.charAt(i) === '/'){
@@ -79,9 +100,14 @@ let handler = (str) => {
         let opt_plus = arr.indexOf('+');
         let opt_sub  = arr.indexOf('-');
         if(arr.indexOf('+') !== -1){
+            console.log('ok')
             res = Number(arr[opt_plus-1]) + Number(arr[opt_plus+1]);
             arr.splice(opt_plus-1,3,res);
         }else{
+            // console.log(`1: ${arr[opt_sub-1]}
+            // 2: ${arr[opt_sub+1]}
+            // 3: ${opt_sub}
+            // 4: arr : ${arr}`)
             res = Number(arr[opt_sub-1]) - Number(arr[opt_sub+1]);
             arr.splice(opt_sub-1,3,res);
         }
@@ -89,4 +115,13 @@ let handler = (str) => {
     return arr[0];
 }
 
+let change = false;
+let result = 0;
+let equal = () => {
+    let arr = document.querySelectorAll("[id='textview']");
+    result = handler(arr[0].value);
+    arr[0].value  = arr[0].value + '=' + result;
+    arr[1].value = result;
+    change = true;
+}
 console.log(handler('-100+1.45454/2+3.'))
